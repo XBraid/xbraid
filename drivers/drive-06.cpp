f6bac2b586425d1382ae65859d0052396832f51c
Jacob Schroder <schroder2@llnl.gov>
2015-12-21 09:50:50 -0800

    One more change to drive-06 regarding running in parallel.  This removes (in large cases 5-10MB) some unnecessary printing to standard out.

--- drivers/drive-06.cpp
+++ drivers/drive-06.cpp
@@ -516,7 +516,10 @@ int main (int argc, char *argv[])
 	NonlinearApp app(&opts, comm_t, pmesh);
    //Run braid
 
-   pmesh->PrintInfo();
+   if (myid == 0)
+   {
+      pmesh->PrintInfo();
+   }
    BraidCore core(comm, &app);
    // Not sure why this doesn't work here...so we assume that the hmin and hmax
    // are based on inline-quad.mesh which has a characteristic size of 0.25
