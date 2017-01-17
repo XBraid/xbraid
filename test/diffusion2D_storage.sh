f102b1d9dbe7441e452993e64bb27177c550d0b8
Matthieu B Lecouvez <lecouvez1@llnl.gov>
2017-01-17 09:38:55 -0800

    Adds regression test for ex-03 with refinement factor larger that coarsening factor.

--- test/diffusion2D_storage.sh
+++ test/diffusion2D_storage.sh
@@ -111,7 +111,13 @@ TESTS=( "$RunString -np 4 $driver_dir/drive-diffusion-2D -pgrid 1 1 4 -nt 256 -m
         "$RunString -np 4 $example_dir/ex-03   -pgrid 1 1 4 -nt 256 -ml 15 -storage 1 -skip 0 -refine -max_rfactor 2" \
         "$RunString -np 4 $example_dir/ex-03   -pgrid 1 1 4 -nt 256 -ml 15 -fmg 1 -storage 1 -skip 0 -refine -max_rfactor 2" \
         "$RunString -np 4 $driver_dir/drive-diffusion-2D -pgrid 1 1 4 -nt 256 -ml 15 -storage 1 -skip 0 -cf0 1" \
-        "$RunString -np 4 $driver_dir/drive-diffusion-2D -pgrid 1 1 4 -nt 256 -ml 15 -storage 1 -skip 0 -cf 4" )
+        "$RunString -np 4 $driver_dir/drive-diffusion-2D -pgrid 1 1 4 -nt 256 -ml 15 -storage 1 -skip 0 -cf 4" \
+        "$RunString -np 4 $example_dir/ex-03   -pgrid 1 1 4 -nt 256 -ml 15 -storage -1 -skip 0 -refine -max_rfactor 3" \
+        "$RunString -np 4 $example_dir/ex-03   -pgrid 1 1 4 -nt 256 -ml 15 -storage -1 -skip 0 -refine -max_rfactor 4" \
+        "$RunString -np 4 $example_dir/ex-03   -pgrid 1 1 4 -nt 256 -ml 15 -storage -1 -skip 0 -refine -max_rfactor 5" \
+        "$RunString -np 4 $example_dir/ex-03   -pgrid 1 1 4 -nt 256 -ml 15 -storage -1 -skip 0 -refine -max_rfactor 6" \
+        "$RunString -np 4 $example_dir/ex-03   -pgrid 1 1 4 -nt 256 -ml 15 -storage -1 -skip 0 -refine -max_rfactor 7" \
+        "$RunString -np 4 $example_dir/ex-03   -pgrid 1 1 4 -nt 256 -ml 15 -storage -1 -skip 0 -refine -max_rfactor 8" )
 
 # The below commands will then dump each of the tests to the output files 
 #   $output_dir/unfiltered.std.out.0, 
