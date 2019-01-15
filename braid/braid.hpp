73e5bab5a291439de6362f0584be92e8fb791b52
Stefanie Guenther <stefanie.guenther@scicomp.uni-kl.de>
2019-01-15 15:14:35 +0100

    Add c++ interface for accessing warm_restart from core.

--- braid/braid.hpp
+++ braid/braid.hpp
@@ -506,6 +506,8 @@ public:
 
    braid_Core GetCore() { return core; };
 
+   braid_Int GetWarmRestart() { return _braid_CoreElt(core, warm_restart); };
+
    void GetDistribution(braid_Int *ilower_ptr, braid_Int *iupper_ptr) { _braid_GetDistribution(core, ilower_ptr, iupper_ptr); };
 
    void Drive() { braid_Drive(core); }
