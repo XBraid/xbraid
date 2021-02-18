65ad41807ebc513cdcf04c1583664614ae51578c
Joshua Christopher <chrisjoshtopher@gmail.com>
2021-02-18 16:25:33 -0700

    Fixed white space and numbers in comments of braid_ASCaller_... definitions

--- braid/braid_status.h
+++ braid/braid_status.h
@@ -681,22 +681,22 @@ ACCESSOR_HEADER_GET1(Objective, Tol,           Real)
  */
 
 /** When CallingFunction equals 0, Braid is in FInterp */
-#define braid_ASCaller_FInterp   0
-/** When CallingFunction equals 0, Braid is in FRestrict */
+#define braid_ASCaller_FInterp 0
+/** When CallingFunction equals 1, Braid is in FRestrict */
 #define braid_ASCaller_FRestrict 1
-/** When CallingFunction equals 0, Braid is in FRefine */
-#define braid_ASCaller_FRefine   2
-/** When CallingFunction equals 0, Braid is in FAccess */
-#define braid_ASCaller_FAccess   3
+/** When CallingFunction equals 2, Braid is in FRefine */
+#define braid_ASCaller_FRefine 2
+/** When CallingFunction equals 3, Braid is in FAccess */
+#define braid_ASCaller_FAccess 3
 /** When CallingFunction equals 4, Braid is inside FRefine after the new finest
  * level has been initialized */
-#define braid_ASCaller_FRefine_AfterInitHier   4
+#define braid_ASCaller_FRefine_AfterInitHier 4
 /** When CallingFunction equals 5, Braid is at the top of the cycle */
-#define braid_ASCaller_Drive_TopCycle   5
-  /** When CallingFunction equals 6, Braid is in FCrelax */
+#define braid_ASCaller_Drive_TopCycle 5
+/** When CallingFunction equals 6, Braid is in FCrelax */
 #define braid_ASCaller_FCRelax 6
-  /** When CallingFunction equals 6, Braid just finished initialization */
-#define braid_ASCaller_Drive_AfterInit   7
+/** When CallingFunction equals 6, Braid just finished initialization */
+#define braid_ASCaller_Drive_AfterInit 7
 
 /** @}*/
 
