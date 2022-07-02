af2614c78cdbe50c83e668c4fb2d153b504478fe
David Alan Vargas <vargas53@ruby964.llnl.gov>
2022-07-02 00:55:25 -0700

    reran with higher iteration limit

--- drivers/experiments/ks/weak_scaling/batch.sh
+++ drivers/experiments/ks/weak_scaling/batch.sh
@@ -23,7 +23,7 @@ ntimes="128 512 2048 8192 32768"
 mlevels="2 3 4 4"
 
 # fixed arguments
-fargs="-tf 4 -nx 128 -cf 4"
+fargs="-tf 4 -nx 128 -cf 4 -mi 50"
 dargs="-theta -Delta -rank 10"
 
 # path to executable
@@ -36,31 +36,31 @@ outd="."
 outn="ks"
 
 # 8 procs
-# nt="128"; nc="8"; ml="2"
-# srun -N 1 -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nt ${nt} -ml 1
-# srun -N 1 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} -theta
-# srun -N 1 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} ${dargs} -Deltalvl 0
+nt="128"; nc="8"; ml="2"
+srun -N 1 -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nt ${nt} -ml 1
+srun -N 1 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} -theta
+srun -N 1 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} ${dargs} -Deltalvl 0
 
 # 32 procs
-# nt="512"; nc="32"; ml="3"
-# srun -N 1 -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nt ${nt} -ml 1
-# srun -N 1 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nt ${nt} -ml 2 -cf0 8
-# srun -N 1 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} -theta
-# srun -N 1 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} ${dargs}
+nt="512"; nc="32"; ml="3"
+srun -N 1 -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nt ${nt} -ml 1
+srun -N 1 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nt ${nt} -ml 2 -cf0 8
+srun -N 1 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} -theta
+srun -N 1 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} ${dargs}
 
 # 128 procs
-# nt="2048"; nc="128"; ml="4"
-# srun -N 1 -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nt ${nt} -ml 1
-# srun -N 3 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nt ${nt} -ml 3 -cf0 8
-# srun -N 3 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} -theta
-# srun -N 3 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} ${dargs}
+nt="2048"; nc="128"; ml="4"
+srun -N 1 -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nt ${nt} -ml 1
+srun -N 3 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nt ${nt} -ml 3 -cf0 8
+srun -N 3 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} -theta
+srun -N 3 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} ${dargs}
 
 # 512 procs
-# nt="8192"; nc="512"; ml="4"
-# srun -N 1  -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nt ${nt} -ml 1
-# srun -N 10 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nt ${nt} -ml 4     -cf0 8
-# srun -N 10 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} -cf0 16 -theta
-# srun -N 10 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} -cf0 16 ${dargs} 
+nt="8192"; nc="512"; ml="4"
+srun -N 1  -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nt ${nt} -ml 1
+srun -N 10 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nt ${nt} -ml 4     -cf0 8
+srun -N 10 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} -cf0 16 -theta
+srun -N 10 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nt ${nt} -ml ${ml} -cf0 16 ${dargs} 
 
 # 2048 procs
 nt="32768"; nc="2048"; ml="5"
