c785c9f2bf06e2e8a00b341126ebb9ff0fbf9992
David Alan Vargas <vargas53@ruby963.llnl.gov>
2022-08-02 10:44:21 -0700

    New strong scaling study for 512 points in space Extended weak scaling with refinement to 4092 cores

--- drivers/experiments/ks/strong_scaling_128/batch.sh
+++ drivers/experiments/ks/strong_scaling_128/batch.sh
@@ -17,7 +17,8 @@ echo -n 'Timestamp START: ';date
 ncores="16 32 64 128 256 512 1024"
 
 # levels
-mlevels="4"
+# mlevels="4"
+mlevels="5"
 
 # coarsening factors
 cfactors="4"
@@ -27,7 +28,8 @@ cfactors="4"
 ranks="2 4 8 10 16 32 64"
 
 # fixed arguments
-fargs="-tf 4 -nt 8192 -cf0 16 -nx 128 -nu 1 -nu0 1 -tol 1e-6 -theta -niters 3 -Deltalvl 1"
+# fargs="-tf 4 -nt 8192 -cf0 16 -nx 128 -nu 1 -nu0 1 -tol 1e-6 -theta -niters 3 -Deltalvl 1"
+fargs="-tf 4 -nt 8192 -cf0 16 -nx 128 -nu 1 -nu0 1 -tol 1e-6 -niters 3"
 
 # path to executable
 ex="../../../drive-ks"
@@ -46,12 +48,12 @@ srun -N 1 -n 1 -o ${outd}/${outn}_ml1 ${ex} ${fargs} -ml 1
 for nc in $ncores; do
    for ml in $mlevels; do
       for cf in $cfactors; do
-         for rank in $ranks; do
-	    echo "srun -N 19 -n ${nc} -o ${outd}/${outn}_Delta${rank}_nc${nc}_cf${cf}_ml${ml}     ${ex} ${fargs} -cf ${cf} -ml ${ml} -Delta -rank ${rank}"
-            srun -N 19 -n ${nc} -o ${outd}/${outn}_Delta${rank}_nc${nc}_cf${cf}_ml${ml}     ${ex} ${fargs} -cf ${cf} -ml ${ml} -Delta -rank ${rank}
-         done
-         echo "srun -N 19 -n ${nc} -o ${outd}/${outn}_nc${nc}_cf${cf}_ml${ml}           ${ex} ${fargs} -cf ${cf} -ml ${ml}"
-         srun -N 19 -n ${nc} -o ${outd}/${outn}_nc${nc}_cf${cf}_ml${ml}           ${ex} ${fargs} -cf ${cf} -ml ${ml}
+         # for rank in $ranks; do
+	    # echo "srun -N 19 -n ${nc} -o ${outd}/${outn}_Delta${rank}_nc${nc}_cf${cf}_ml${ml}     ${ex} ${fargs} -cf ${cf} -ml ${ml} -Delta -rank ${rank}"
+            # srun -N 19 -n ${nc} -o ${outd}/${outn}_Delta${rank}_nc${nc}_cf${cf}_ml${ml}     ${ex} ${fargs} -cf ${cf} -ml ${ml} -Delta -rank ${rank}
+         # done
+         # echo "srun -N 19 -n ${nc} -o ${outd}/${outn}_nc${nc}_cf${cf}_ml${ml}           ${ex} ${fargs} -cf ${cf} -ml ${ml}"
+         # srun -N 19 -n ${nc} -o ${outd}/${outn}_nc${nc}_cf${cf}_ml${ml}           ${ex} ${fargs} -cf ${cf} -ml ${ml}
          # mpirun -n ${nc} ${ex} ${fargs} -ml ${ml} -cf ${cf} -Delta > ${outd}/${outn}_Delta_nc${nc}_cf${cf}_ml${ml}
          # mpirun -n ${nc} ${ex} ${fargs} -ml ${ml} -cf ${cf}        > ${outd}/${outn}_nc${nc}_cf${cf}_ml${ml}
       done
--- drivers/experiments/ks/weak_scaling_refinement/batch.sh
+++ drivers/experiments/ks/weak_scaling_refinement/batch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ##### These lines are for SLURM
-#SBATCH -N 19
+#SBATCH -N 76
 #SBATCH -p pbatch
 #SBATCH -A paratime
 #SBATCH -t 30
@@ -14,13 +14,13 @@ echo -n 'My jobid is '; echo $SLURM_JOBID
 echo -n 'Timestamp START: '; date
 
 # number of cores
-ncores="8 64 512"
+ncores="8 64 512 4096"
 
 # timegrid size
-ntimes="128 512 2048"
+ntimes="128 512 2048 8192"
 
 # levels
-mlevels="2 3 4 4"
+mlevels="2 3 4 5"
 
 # fixed arguments
 fargs="-tf 4 -nx 128 -cf 4"
@@ -36,32 +36,32 @@ outd="."
 outn="ks"
 
 # 8 procs
-nx="128"; nt="128"; nc="8"; ml="2"
-srun -N 1 -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml 1
-srun -N 1 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} -cf0 2
-srun -N 1 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} -theta
-srun -N 1 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} ${dargs} -Deltalvl 0
+# nx="128"; nt="128"; nc="8"; ml="2"
+# srun -N 1 -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml 1
+# srun -N 1 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} -cf0 2
+# srun -N 1 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} -theta
+# srun -N 1 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} ${dargs} -Deltalvl 0
 
 # 64 procs
-nx="256"; nt="512"; nc="64"; ml="3"
-srun -N 1 -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml 1
-srun -N 2 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml 2 -cf0 8
-srun -N 2 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} -theta
-srun -N 2 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} ${dargs}
+# nx="256"; nt="512"; nc="64"; ml="3"
+# srun -N 1 -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml 1
+# srun -N 2 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml 2 -cf0 8
+# srun -N 2 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} -theta
+# srun -N 2 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} ${dargs}
 
 # 512 procs
-nx="512"; nt="2048"; nc="512"; ml="4"
-srun -N 1  -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml 1
-srun -N 10 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml 3 -cf0 8
-srun -N 10 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} -theta
-srun -N 10 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} ${dargs}
+# nx="512"; nt="2048"; nc="512"; ml="4"
+# srun -N 1  -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml 1
+# srun -N 10 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml 3 -cf0 8
+# srun -N 10 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} -theta
+# srun -N 10 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} ${dargs}
 
 # 4096 procs
 nx="1024"; nt="8192"; nc="4096"; ml="5"
 srun -N 1  -n 1     -o ${outd}/${outn}_nc${nc}_ml1           ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml 1
-srun -N 10 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml 4 -cf0 8
-srun -N 10 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} -theta
-srun -N 10 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} ${dargs}
+srun -N 76 -n ${nc} -o ${outd}/${outn}_nc${nc}_ml${ml}       ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml 4 -cf0 8
+srun -N 76 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} -theta
+srun -N 76 -n ${nc} -o ${outd}/${outn}_Delta_nc${nc}_ml${ml} ${ex} ${fargs} -nx ${nx} -nt ${nt} -ml ${ml} ${dargs}
 
 echo 'Done'
 echo -n 'Timestamp END: ';date
