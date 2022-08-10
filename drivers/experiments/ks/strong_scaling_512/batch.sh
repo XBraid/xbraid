0878bc3ffc2828eb1c92269b5697a83c176d3888
David Alan Vargas <vargas53@ruby967.llnl.gov>
2022-08-10 07:14:40 -0700

    Tried running strong scaling with some optimized settings for the spatial solver

--- drivers/experiments/ks/strong_scaling_512/batch.sh
+++ drivers/experiments/ks/strong_scaling_512/batch.sh
@@ -21,7 +21,7 @@ ranks="2 4 8 9 16 32 64"
 
 # fixed arguments
 fargs="-tf 4 -nt 8192 -nx 512 -cf 4 -cf0 16 -nu 1 -nu0 1 -tol 1e-8 -niters 2 -mi 25"
-dargs="-theta -Delta -Deltalvl 1 -cglv"
+dargs="-theta -Delta -Deltalvl 1"
 
 # path to executable
 ex="../../../drive-ks"
@@ -34,19 +34,19 @@ outn="ks"
 
 # serial run
 # mpirun -n 1 ${ex} ${fargs} -ml 1 > ${outd}/${outn}_ml1
-# echo "srun -N 1 -n 1 -o ${outd}/${outn}_ml1 ${ex} ${fargs} -ml 1"
-# srun -N 1 -n 1 -o       ${outd}/${outn}_ml1 ${ex} ${fargs} -ml 1
+echo "srun -N 1 -n 1 -o ${outd}/${outn}_ml1 ${ex} ${fargs} -ml 1"
+srun -N 1 -n 1 -o       ${outd}/${outn}_ml1 ${ex} ${fargs} -ml 1
 
 for nc in $ncores; do
-   echo "srun -N 19 -n ${nc} -o ${outd}/${outn}_nc${nc}       ${ex} ${fargs} -ml 2"
-   srun  -N 19 -n ${nc} -o ${outd}/${outn}_nc${nc}            ${ex} ${fargs} -ml 2
+   #	echo "srun -N 19 -n ${nc} -o ${outd}/${outn}_nc${nc}       ${ex} ${fargs} -ml 2"
+   #	srun  -N 19 -n ${nc} -o ${outd}/${outn}_nc${nc}            ${ex} ${fargs} -ml 2
 
-   echo "srun -N 19 -n ${nc} -o ${outd}/${outn}_theta_nc${nc} ${ex} ${fargs} -ml 3 -theta"
-   srun -N 19 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}       ${ex} ${fargs} -ml 3 -theta
+   #	echo "srun -N 19 -n ${nc} -o ${outd}/${outn}_theta_nc${nc} ${ex} ${fargs} -ml 3 -theta"
+   #	srun -N 19 -n ${nc} -o ${outd}/${outn}_theta_nc${nc}       ${ex} ${fargs} -ml 3 -theta
 
    for rank in $ranks; do
-      echo "srun -N 19 -n ${nc} -o ${outd}/${outn}_Delta${rank}_nc${nc} ${ex} ${fargs} -ml 3 {dargs} -rank ${rank}"
-      srun -N 19 -n ${nc} -o ${outd}/${outn}_Delta${rank}_nc${nc}       ${ex} ${fargs} -ml 3 {dargs} -rank ${rank} 
+      echo "srun -N 19 -n ${nc} -o ${outd}/${outn}_Delta${rank}_nc${nc} ${ex} ${fargs} -ml 3 ${dargs} -rank ${rank}"
+      srun -N 19 -n ${nc} -o ${outd}/${outn}_Delta${rank}_nc${nc}       ${ex} ${fargs} -ml 3 ${dargs} -rank ${rank} 
    done
    for rank in $ranks; do
       echo "srun -N 19 -n ${nc} -o ${outd}/${outn}_Delta${rank}_nc${nc}_cf16 ${ex} ${fargs} -ml 3 ${dargs} -niters 3 -cf 16 -rank ${rank}"
