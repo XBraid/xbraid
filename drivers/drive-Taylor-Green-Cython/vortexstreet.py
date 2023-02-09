cedde930a402db6d1ccbe01d574fa12ba8122795
David <dvargas2@unm.edu>
2023-02-09 12:33:41 -0700

    minor changes to vortexstreet.py

--- drivers/drive-Taylor-Green-Cython/vortexstreet.py
+++ drivers/drive-Taylor-Green-Cython/vortexstreet.py
@@ -8,41 +8,44 @@ from phi.torch.flow import *
 import matplotlib.pyplot as plt
 # from phi.tf.flow import *
 # from phi.jax.flow import *
-assert backend.default_backend().set_default_device('GPU')
+# assert backend.default_backend().set_default_device('GPU')
 
 SPEED = 20.
 OFFSET = 1e-6
-v0 = tensor([(SPEED, 0), (SPEED + OFFSET, 0)], batch("speed"), channel(vector='x,y'))
+v0 = tensor([(SPEED, 0), (SPEED, 0)], batch("init"), channel(vector='x,y'))
 
-nx, ny = (256, 128)
-DOMAIN = {"extrapolation": extrapolation.BOUNDARY, 'x': nx, 'y': ny, "bounds": Box(x=128, y=64)}
+nx, ny = (64, 64)
+DOMAIN = {"extrapolation": extrapolation.PERIODIC, 'x': nx, 'y': ny, "bounds": Box(x=20, y=20)}
 velocity = StaggeredGrid(v0, **DOMAIN)
 noise = StaggeredGrid(Noise(), **DOMAIN)
 velocity += noise
-CYLINDER = Obstacle(geom.infinite_cylinder(x=15, y=32, radius=5, inf_dim=None))
+CYLINDER = Obstacle(geom.infinite_cylinder(x=10, y=10, radius=3, inf_dim=None))
+BOX = Obstacle(geom.Cuboid(x=10, y=10))
 BOUNDARY_MASK = StaggeredGrid(Box(x=(-INF, 0.5), y=None), velocity.extrapolation, velocity.bounds, velocity.resolution)
-vort = field.curl(velocity)
+vort = field.curl(velocity.with_extrapolation(extrapolation.BOUNDARY))
 pressure = None
 
 @jit_compile  # Only for PyTorch, TensorFlow and Jax
 def step(v, p, dt=.1):
     v = advect.semi_lagrangian(v, v, dt)
-    v = v * (1 - BOUNDARY_MASK) + BOUNDARY_MASK * (SPEED, 0)
-    return fluid.make_incompressible(v, [CYLINDER], Solve('auto', 1e-5, 0, x0=p))
+    # v = v * (1 - BOUNDARY_MASK) + BOUNDARY_MASK * (SPEED, 0)
+    v, p = fluid.make_incompressible(v, [CYLINDER], Solve('auto', 1e-5, 0, x0=p))
+    # v, p = fluid.make_incompressible(v, [BOX], Solve('auto', 1e-5, 0, x0=p))
+    return v, p
 
 diff = []
 cmap="plasma"
 fig, (ax1, ax2, ax3) = plt.subplots(3,1)
-im1 = ax1.imshow(vort.speed[0].values.numpy('y,x'), cmap=cmap, vmin=-2*SPEED, vmax=2*SPEED)
+im1 = ax1.imshow(vort.init[0].values.numpy('y,x'), cmap=cmap, vmin=-2*SPEED, vmax=2*SPEED)
 line = ax3.semilogy([])
 
-for _ in range(100):
-    for _ in range(10):
+for _ in range(200):
+    for _ in range(1):
         velocity, pressure = step(velocity, pressure)
-    diff.append(field.l2_loss(velocity.speed[0] - velocity.speed[1])/math.sqrt(nx*ny*2))
-    vort = field.curl(velocity)
-    vort_diff = vort.speed[0].values.numpy('y,x') - vort.speed[1].values.numpy('y,x')
-    im1.set_data(vort.speed[0].values.numpy('y,x'))
+    diff.append(field.l2_loss(velocity.init[0] - velocity.init[1])/math.sqrt(nx*ny*2))
+    vort = field.curl(velocity.with_extrapolation(extrapolation.BOUNDARY))
+    vort_diff = vort.init[0].values.numpy('y,x') - vort.init[1].values.numpy('y,x')
+    im1.set_data(vort.init[0].values.numpy('y,x'))
     ax2.imshow(vort_diff)
     ax3.semilogy(diff)
     plt.pause(0.01)
